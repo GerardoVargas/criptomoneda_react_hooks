72f6608134e53d5f6e63539bebc35672a1a0b45e
Gerardo Vargas <evc414@gmail.com>
2020-05-09 02:34:57 -0500

    Contenedor & Image added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,35 @@
 import React from 'react';
 import './App.css';
+import styled from '@emotion/styled'
+import imagen from './cryptomonedas.png'
+
+const Contenedor = styled.div`
+  max-width: 900px;
+  margin: 0 auto;
+  @media (min-width: 992px) {
+    display: grid;
+    grid-template-columns: repeat(2, 1fr);
+    column-gap: 2rem;
+  }
+`
+const Imagen = styled.img`
+  max-width: 100%;
+  margin-top: 5rem;
+`
 
 function App() {
   return (
-   <h1>Criptomonedas React</h1>
+   <Contenedor>
+     <div>
+        <Imagen
+            src={imagen}
+            alt="Imagen Crypto"
+        />
+     </div>
+     <div>
+
+     </div>
+   </Contenedor>
   );
 }
 
