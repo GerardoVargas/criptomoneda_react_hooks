025b26b95a9b667ccdc38182c8c4613db19284e8
Gerardo Vargas <evc414@gmail.com>
2020-05-09 02:41:01 -0500

    Heading component added

--- src/App.js
+++ src/App.js
@@ -16,6 +16,23 @@ const Imagen = styled.img`
   max-width: 100%;
   margin-top: 5rem;
 `
+const Heading = styled.h1`
+  font-family: "Bebas Neue", cursive;
+  color: white;
+  text-align: left;
+  font-weight: 700;
+  font-size: 50px;
+  margin-bottom: 50px;
+  margin-top: 80px;
+
+  &::after{
+    content: '';
+    width: 100px;
+    height: 6px;
+    background-color: #66a2fe;
+    display: block;
+  }
+`
 
 function App() {
   return (
@@ -27,7 +44,7 @@ function App() {
         />
      </div>
      <div>
-
+        <Heading>Cotiza criptomonedas al instante</Heading>
      </div>
    </Contenedor>
   );
