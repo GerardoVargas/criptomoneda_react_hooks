8d0698a6c74ada9620984f0ff48955d67c18eebd
Gerardo Vargas <evc414@gmail.com>
2020-05-10 02:37:45 -0500

    Error component added

--- src/components/Formulario.js
+++ src/components/Formulario.js
@@ -1,6 +1,6 @@
 import React, {useEffect, useState} from 'react';
 import styled from '@emotion/styled'
-
+import Error from './Error'
 import useMoneda from '../hooks/useMoneda'
 import useCriptomoneda from '../hooks/useCriptomoneda'
 import Axios from 'axios';
@@ -76,7 +76,7 @@ const Formulario = () => {
             onSubmit={cotizarMoneda}
         >
 
-            {error ? 'Hay un error!' : null}
+            {error ? <Error mensaje="Todos los campos son obligatorios!" /> : null}
 
             <SelectMonedas/>
 
