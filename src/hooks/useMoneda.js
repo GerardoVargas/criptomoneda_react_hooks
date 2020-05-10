c06ccbcf5ea9d2b00314aa377a910e4176cf6ee3
Gerardo Vargas <evc414@gmail.com>
2020-05-09 22:48:50 -0500

    useMoneda styles added

--- src/hooks/useMoneda.js
+++ src/hooks/useMoneda.js
@@ -1,4 +1,27 @@
 import React, {Fragment, useState} from 'react';
+import styled from '@emotion/styled'
+
+
+const Label = styled.label`
+    font-family: "Bebas Neue", cursive;
+    color: white;
+    text-transform: uppercase;
+    font-weight: bold;
+    font-size: 2.4rem;
+    margin-top: 2rem;
+    display: block;
+`
+const Select = styled.select`
+    width: 100%;
+    display: block;
+    padding: 1rem;
+    -moz-appearance: none;
+    border-radius: 10px;
+    border: none;
+    font-size: 1.2rem;
+`
+
+
 
 const useMoneda = (label, stateInicial, opciones) => {
 
@@ -7,7 +30,7 @@ const useMoneda = (label, stateInicial, opciones) => {
 
     const Seleccionar = () => (
         <Fragment>
-            <label>{label}}</label>
+            <Label>{label}</Label>
             <select
                 onChange={ e => actualizarState(e.target.value)}
                 value={state}
